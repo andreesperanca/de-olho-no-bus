d8d5dad99cba36fe1a9ec0e2f7bf41d3d00d0e5a
andreesperanca <andreluizesperancacorreia@gmail.com>
2022-09-23 17:43:39 -0300

    refactor: refactor code

--- app/src/main/java/com/andreesperanca/deolhonobus/util/ConstantsProject.kt
+++ app/src/main/java/com/andreesperanca/deolhonobus/util/ConstantsProject.kt
@@ -1,5 +1,5 @@
-package com.andreesperanca.deolhonobus.util
+    package com.andreesperanca.deolhonobus.util
 
 const val BASE_URL = "http://api.olhovivo.sptrans.com.br/v2.1/"
-const val API_KEY = "f6689d20403fc15a7d317d209e659599943870420341a475bfe1fb814ad20cbb"
+const val API_KEY = "SUA_API_KEY"
 
